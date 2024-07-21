7eaa07c8965eb0059554cd5b065480e0b6e69b26
theteachr <sop.theteachr@gmail.com>
2024-07-21 16:24:33 +0530

    :rotating_light: Let web code be just a placeholder

--- web/card.ml
+++
@@ -1,8 +0,0 @@
-include Card
-
-let show = ()
-
-  (*            function *)
-  (* | Money money -> Money.show money *)
-  (* | Property property -> Property.show_card property *)
-  (* | Action action -> Action.show action *)
--- web/main.ml
+++ web/main.ml
@@ -1,6 +1,1 @@
-let () =
-  [ "theteachr"; "injuly"; "kaus"; "manas" ]
-  |> List.map Player.make
-  |> Fortune.Game.start
-  |> Show.game
-  |> print_endline
+let () = print_endline "Hello, Web!"
