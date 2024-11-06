e8b17f14431ca3caca486078190ecadce1ca01da
theteachr <sop.theteachr@gmail.com>
2024-11-06 17:00:30 +0530

    Remove duplicate renders in expect tests

--- test/test_fortune.ml
+++ test/test_fortune.ml
@@ -13,99 +13,10 @@ let game deck =
 
 let render game = game |> Tui.show |> print_string
 let shuffled_deck = Deck.(shuffle ~seed:10 default)
-
-let%expect_test "remove_from_hand works" =
-  let game = game Deck.default in
-  let player = Game.current_player game in
-  Tui.Player.show_hand player.hand |> print_endline;
-  [%expect {|
-    0. BLUE
-    1. BLUE
-    2. BROWN
-    3. BROWN
-    4. GREEN |}];
-  let _, player = Player.use_card 0 player in
-  Tui.Player.show_hand player.hand |> print_endline;
-  [%expect {|
-    0. BLUE
-    1. BROWN
-    2. BROWN
-    3. GREEN |}]
+let default_game = game shuffled_deck
 
 let%expect_test "default game" =
-  let game = game Deck.default in
-  render game;
-  [%expect
-    {|
-    ocaml
-
-    Hand -
-
-    0. BLUE
-    1. BLUE
-    2. BROWN
-    3. BROWN
-    4. GREEN
-
-    Bank -
-
-
-
-    Properties -
-
-
-
-    86 card(s) left in the deck.
-    |}];
-  let game = Game.play_card 0 game in
-  render game;
-  [%expect
-    {|
-    ocaml
-
-    Hand -
-
-    0. BLUE
-    1. BROWN
-    2. BROWN
-    3. GREEN
-
-    Bank -
-
-
-
-    Properties -
-
-    0. BLUE
-
-    86 card(s) left in the deck.
-    |}];
-  game |> Game.play_card 2 |> render;
-  [%expect
-    {|
-    ocaml
-
-    Hand -
-
-    0. BLUE
-    1. BROWN
-    2. GREEN
-
-    Bank -
-
-
-
-    Properties -
-
-    0. BROWN
-    1. BLUE
-
-    86 card(s) left in the deck.
-    |}]
-
-let%expect_test "playing a money card should work" =
-  let game = game shuffled_deck in
-  render game;
+  render default_game;
   [%expect
     {|
     ocaml
@@ -126,8 +37,55 @@ let%expect_test "playing a money card should work" =
 
 
 
+    86 card(s) left in the deck.
+    |}]
+
+let%expect_test "play simple property cards" =
+  let game = game Deck.default in
+  render game;
+  [%expect {|
+    ocaml
+
+    Hand -
+
+    0. BLUE
+    1. BLUE
+    2. BROWN
+    3. BROWN
+    4. GREEN
+
+    Bank -
+
+
+
+    Properties -
+
+
+
     86 card(s) left in the deck. |}];
-  game |> Game.play_card 1 |> render;
+  Game.(game |> play_card 0 |> play_card 2) |> render;
+  [%expect {|
+    ocaml
+
+    Hand -
+
+    0. BLUE
+    1. BROWN
+    2. GREEN
+
+    Bank -
+
+
+
+    Properties -
+
+    0. BROWN
+    1. BLUE
+
+    86 card(s) left in the deck. |}]
+
+let%expect_test "play a money card" =
+  default_game |> Game.play_card 1 |> render;
   [%expect
     {|
     ocaml
@@ -150,31 +108,8 @@ let%expect_test "playing a money card should work" =
     86 card(s) left in the deck.
     |}]
 
-let%expect_test "playing an action card as money should work" =
-  let game = game shuffled_deck in
-  render game;
-  [%expect
-    {|
-    ocaml
-
-    Hand -
-
-    0. SKYBLUE BROWN
-    1. M4
-    2. RENT: BROWN SKYBLUE
-    3. PASS GO
-    4. JUST SAY NO
-
-    Bank -
-
-
-
-    Properties -
-
-
-
-    86 card(s) left in the deck. |}];
-  Game.(game |> play_money 4 |> play_money 3) |> render;
+let%expect_test "play an action card as money" =
+  Game.(default_game |> play_money 4 |> play_money 3) |> render;
   [%expect
     {|
     ocaml
